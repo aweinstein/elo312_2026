7007107068e12b4f630b8c7e4430b83cbeec581b
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2026-08-16 16:22:34 -0400

    Vincula UART con printf.

--- sg1/Core/Src/main.c
+++ sg1/Core/Src/main.c
@@ -220,7 +220,10 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-
+int _write(int file, char *ptr, int len) {
+  HAL_UART_Transmit(&huart2, (uint8_t *)ptr, len, HAL_MAX_DELAY);
+  return len;
+}
 /* USER CODE END 4 */
 
 /**
